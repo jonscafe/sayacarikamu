9bf8acdff2a462e105657878e1e294196b8a3100
jonscafe <68538183+jonscafe@users.noreply.github.com>
2024-02-06 22:42:04 +0700

    Update sinta.py

--- sinta.py
+++ sinta.py
@@ -1,10 +1,10 @@
-import requests
+import streamlit as st
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 def sinta(user_input):
-    print('SINTA AUTHOR')
-    print('=========================')
+    st.write('SINTA AUTHOR')
+    st.write('=========================')
     session = HTMLSession()
     search_url = f'https://sinta.kemdikbud.go.id/authors/?q={user_input}'
 
@@ -15,18 +15,19 @@ def sinta(user_input):
     profile_links = search_response.html.find('a[href*="/authors/profile/"]')
 
     if not profile_links:
-        print('Tidak ditemukan!')
+        st.write('Tidak ditemukan!')
     else:
         for profile_link in profile_links:
             # Extracting name from the opened link
-            profile_response = requests.get(profile_link.absolute_links.pop())
+            profile_response = session.get(profile_link.absolute_links.pop())
             profile_soup = BeautifulSoup(profile_response.content, 'html.parser')
             name_element = profile_soup.find('h3').find('a')
             name = name_element.text.strip() if name_element else 'Tidak ditemukan!'
 
-            print(f'Name: {name}, Link: {profile_link.absolute_links.pop()}')
+            st.write(f'Name: {name}, Link: {profile_link.absolute_links.pop()}')
 
-    print('=========================')
+    st.write('=========================')
+
 
 if __name__ == "__main__":
     user_input = input("Input Name: ")
