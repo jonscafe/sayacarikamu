b4b080b17492ea47f91333587d3f8a1018e99192
jonscafe <68538183+jonscafe@users.noreply.github.com>
2024-02-06 22:47:18 +0700

    Update pddikti.py

--- pddikti.py
+++ pddikti.py
@@ -1,36 +1,35 @@
 import streamlit as st
-from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
 
 def pddikti(user_input):
     st.write('PDDIKTI')
     st.write('=========================')
-    session = HTMLSession()
     search_url = f'https://pddikti.kemdikbud.go.id/search/{user_input}'
 
-    search_response = session.get(search_url)
-    search_response.html.render(timeout=10)
-    
-    mahasiswa_links = search_response.html.find('a[href^="/data_mahasiswa/"]')
+    search_response = requests.get(search_url)
+    soup = BeautifulSoup(search_response.content, 'html.parser')
+
+    mahasiswa_links = soup.find_all('a', href=lambda href: href and href.startswith('/data_mahasiswa/'))
 
     for link in mahasiswa_links[:5]:
-        link_url = link.absolute_links.pop()
-        link_response = session.get(link_url)
-        link_response.html.render(timeout=10)
-        link_html_lower = link_response.html.text.lower()
+        link_url = f"https://pddikti.kemdikbud.go.id{link['href']}"
+        link_response = requests.get(link_url)
+        link_soup = BeautifulSoup(link_response.content, 'html.parser')
+        link_html_lower = link_soup.text.lower()
         if user_input.lower() in link_html_lower:
             st.write(f'{user_input}, PDDIKTI Mahasiswa: {link_url}')
     
-    dosen_links = search_response.html.find('a[href^="/data_dosen/"]')
+    dosen_links = soup.find_all('a', href=lambda href: href and href.startswith('/data_dosen/'))
     for link in dosen_links[:5]:
-        link_url = link.absolute_links.pop()
-        link_response = session.get(link_url)
-        link_response.html.render(timeout=10)
-        link_html_lower = link_response.html.text.lower()
+        link_url = f"https://pddikti.kemdikbud.go.id{link['href']}"
+        link_response = requests.get(link_url)
+        link_soup = BeautifulSoup(link_response.content, 'html.parser')
+        link_html_lower = link_soup.text.lower()
         if user_input.lower() in link_html_lower:
             st.write(f'{user_input}, PDDIKTI Dosen: {link_url}')
     
     st.write('=========================')
 
-    
 if __name__ == "__main__":
     pddikti()
