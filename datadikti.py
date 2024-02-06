f78a8b2d029da3abb717b748198dbcd310b5795e
jonscafe <68538183+jonscafe@users.noreply.github.com>
2024-02-06 22:38:08 +0700

    Update datadikti.py

--- datadikti.py
+++ datadikti.py
@@ -23,3 +23,28 @@ def datadikti(user_input):
 if __name__ == "__main__":
     datadikti()
 
+import streamlit as st
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+def datadikti(user_input):
+    st.write('DATADIKTI')
+    st.write('=========================')
+    session = HTMLSession()
+    input_lower = user_input.lower()
+    formatted_input = input_lower.replace(" ", "-")
+
+    search_url = f'https://www.datadikti.com/searches/{formatted_input}'
+    search_response = session.get(search_url)
+    
+    soup = BeautifulSoup(search_response.text, 'html.parser')
+
+    data_mahasiswa = soup.find_all('li')
+
+    for item in data_mahasiswa:
+        st.write(item.text)
+    
+    st.write('=========================')
+
+if __name__ == "__main__":
+    datadikti()
