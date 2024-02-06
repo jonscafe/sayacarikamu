065f679da12ea1878590204b57298a46b68447f8
jonscafe <68538183+jonscafe@users.noreply.github.com>
2024-02-06 22:41:22 +0700

    Update scholar.py

--- scholar.py
+++ scholar.py
@@ -1,13 +1,13 @@
+import streamlit as st
 from requests_html import HTMLSession
 
 def scholar(user_input):
-    print('SCHOLAR')
-    print('=========================')
+    st.write('SCHOLAR')
+    st.write('=========================')
     session = HTMLSession()
     search_url = f'https://scholar.google.com/citations?view_op=search_authors&mauthors={user_input}&hl=id&oi=ao'
 
     search_response = session.get(search_url)
-    print(search_response)
     search_response.html.render(timeout=10)
     user_input_lower = user_input.lower()
     info_author = search_response.html.text.lower()
@@ -20,15 +20,16 @@ def scholar(user_input):
         if ' Afliasi: ' in name_and_affiliation_line:
             name_index = name_and_affiliation_line.find(' Afliasi: ')
             name = name_and_affiliation_line[:name_index].strip()
-            print('Nama:', name)
+            st.write('Nama:', name)
         else:
-            print('Nama:', name_and_affiliation_line)
+            st.write('Nama:', name_and_affiliation_line)
 
-        print('Link Scholar:', search_response.url)
+        st.write('Link Scholar:', search_response.url)
     else:
-        print(f'No information found for {user_input}')
+        st.write(f'No information found for {user_input}')
     
-    print('=========================')
+    st.write('=========================')
+
     
 if __name__ == "__main__":
     scholar()
